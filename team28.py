3ba52a3ea0c1830d6c08dcb276a670ff979c2d89
tshaark <sarthak.singh@students.iiit.ac.in>
2019-03-01 16:18:08 +0530

    a very few changes in the minimax function

--- team28.py
+++ team28.py
@@ -24,34 +24,34 @@ class Player28:
         return self.next_move
 
     
-    def minimax(self,board,old_move,depth,alpha,beta,max_player,symbol):
+    def minimax(self,board, old_move, depth, alpha, beta, max_player, symbol):
         status = board.find_terminal_state()
-        if time.time()-self.timer>=23:
-            return self.heuristic()
+        if time.time() - self.timer>=23:
+            return self.heuristic(board,old_move)
         if depth == 3 or status[0] != 'CONTINUE':
             if self.symbol == 'x':
                 return self.heuristic(board, old_move)
             else:
-                return (0 - self.heuristic(board, old_move))
+                return (-1*self.heuristic(board, old_move))
         if max_player:
             value = self.ninfinity
             self.available_cells = board.find_valid_move_cells(old_move)
             random.shuffle(self.available_cells)
-            for move in self.available_cells:
-                board.update(old_move, move, symbol)
-                if symbol == 'x':
-                    next_flag = 'o'
-                else:
+            for move_cell in self.available_cells:
+                board.update(old_move, move_cell, symbol)
+                if symbol == 'o':
                     next_flag = 'x'
-                child_value = self.minimax(board, move, depth + 1, alpha, beta, False, next_flag)
-                board.big_boards_status[move[0]][move[1]][move[2]] = '-'
-                board.small_boards_status[move[0]][move[1] / 3][move[2] / 3] = '-'
+                else:
+                    next_flag = 'o'
+                child_value = self.minimax(board, move_cell, depth + 1, alpha, beta, False, next_flag)
+                board.big_boards_status[move_cell[0]][move_cell[1]][move_cell[2]] = '-'
+                board.small_boards_status[move_cell[0]][move_cell[1] / 3][move_cell[2] / 3] = '-'
                 if child_value > value:
                     value = child_value
                     if depth == 0:
-                        self.next_move = copy.deepcopy(move)
+                        self.next_move = copy.deepcopy(move_cell)
                 alpha = max(alpha, value)				
-                if beta <= alpha:
+                if alpha >= beta:
                     break
             return value
 
@@ -59,21 +59,21 @@ class Player28:
             value = self.infinity
             self.available_cells = board.find_valid_move_cells(old_move)
             random.shuffle(self.available_cells)
-            for move in self.available_cells:
-                board.update(old_move, move, symbol)
-                if symbol == 'x':
-                    next_flag = 'o'
-                else:
+            for move_cell in self.available_cells:
+                board.update(old_move, move_cell, symbol)
+                if symbol == 'o':
                     next_flag = 'x'
-                child_value = self.minimax(board, move, depth + 1, alpha, beta, True, next_flag)
-                board.big_boards_status[move[0]][move[1]][move[2]] = '-'
-                board.small_boards_status[move[0]][move[1] / 3][move[2] / 3] = '-'
+                else:
+                    next_flag = 'o'
+                child_value = self.minimax(board, move_cell, depth + 1, alpha, beta, True, next_flag)
+                board.big_boards_status[move_cell[0]][move_cell[1]][move_cell[2]] = '-'
+                board.small_boards_status[move_cell[0]][move_cell[1] / 3][move_cell[2] / 3] = '-'
                 if child_value < value:
                     value = child_value
                     if depth == 0:
-                        self.next_move = copy.deepcopy(move)
+                        self.next_move = copy.deepcopy(move_cell)
                 beta = min(beta, value)				
-                if beta <= alpha:
+                if alpha >= beta:
                     break
             return value
             
